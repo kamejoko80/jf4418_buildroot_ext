3784e42e4cad24b4f49c70a44882f2cf59ff2ac0
phuongminh.dang@gmail.com <phuongminh.dang@gmail.com>
2016-11-08 10:55:23 +0700

    added travis-ci build

---
+++ travis_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/pdtechvn/uboot-mkimage.git
+cd uboot-mkimage
+make
+export PATH=$PATH:$PWD
+cd ../
+
+echo [Build Rootfs...]
+git clone git://git.buildroot.net/buildroot
+cd buildroot
+git checkout 2015.11.x
+make BR2_EXTERNAL=../ jf4418_defconfig
+make -j8
+
+
+
